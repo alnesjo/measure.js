a89ca737aca2c31492f90551cc70c6f1cc62151b
Robert Alnesjö <alnesjo@hotmail.com>
2018-02-12 15:17:22 +0100

    Update prefix.js

--- src/prefix.js
+++ src/prefix.js
@@ -1,4 +1,20 @@
-export const kilo = {valueOf: () => 3, toString: () => 'k'};
-export const none = {valueOf: () => 0, toString: () => ''};
-export const centi = {valueOf: () => -2, toString: () => 'c'};
-export const milli = {valueOf: () => -3, toString: () => 'm'};
+const short = Symbol('short');
+const power = Symbol('power');
+
+class Prefix {
+    constructor(s,p) {
+        this[short] = s;
+        this[power] = p;
+    }
+    valueOf() {
+        return this[power];
+    }
+    toString() {
+        return this[short];
+    }
+}
+
+export const kilo = new Prefix('k', 3);
+export const none = new Prefix('', 0);
+export const centi = new Prefix('c', -2);
+export const milli = new Prefix('m', -3);
